cc1ab8350fa90a73d00ca67eab029e406a0fca91
Justin Tychek <jmtychek@iu.edu>
2022-02-22 14:42:12 -0500

    Lab 7 Setup

---
+++ labs/lab7/ex1/js/app.js
@@ -0,0 +1,5 @@
+//Justin Tychek
+//2/16/2022
+
+//define the ID as a variable
+let divNumb = document.getElementById("divNumb");
---
+++ labs/lab7/ex2/js/app.js
@@ -0,0 +1,5 @@
+//Justin Tychek
+//2/16/2022
+
+//define the ID as a variable
+let divBeep = document.getElementById("divBeep");
