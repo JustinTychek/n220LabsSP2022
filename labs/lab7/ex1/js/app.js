fa00150a4eff80348662890ecc54f6344dc97eb4
Justin Tychek <jmtychek@iu.edu>
2022-02-23 16:28:55 -0500

    Exercise 1 Finished

--- labs/lab7/ex1/js/app.js
+++ labs/lab7/ex1/js/app.js
@@ -1,5 +1,53 @@
 //Justin Tychek
 //2/16/2022
 
-//define the ID as a variable
-let divNumb = document.getElementById("divNumb");
+//define the IDs as a variable
+let banana = document.getElementById("banana");
+let apple = document.getElementById("apple");
+let strawberry = document.getElementById("strawberry");
+let grape = document.getElementById("grape");
+let pear = document.getElementById("pear");
+let cherry = document.getElementById("cherry");
+
+//create functions that highlight the divs and makes the text bold
+function clickBanana() {
+  //change the text to bold
+  banana.style.fontWeight = "bold";
+  //hightlight the div
+  banana.style.backgroundColor = "#ebe534";
+}
+
+function clickApple() {
+  //change the text to bold
+  apple.style.fontWeight = "bold";
+  //hightlight the div
+  apple.style.backgroundColor = "#f02b38";
+}
+
+function clickStrawberry() {
+  //change the text to bold
+  strawberry.style.fontWeight = "bold";
+  //hightlight the div
+  strawberry.style.backgroundColor = "#ff6b93";
+}
+
+function clickGrape() {
+  //change the text to bold
+  grape.style.fontWeight = "bold";
+  //hightlight the div
+  grape.style.backgroundColor = "#762a96";
+}
+
+function clickPear() {
+  //change the text to bold
+  pear.style.fontWeight = "bold";
+  //hightlight the div
+  pear.style.backgroundColor = "#6bd16e";
+}
+
+function clickCherry() {
+  //change the text to bold
+  cherry.style.fontWeight = "bold";
+  //hightlight the div
+  cherry.style.backgroundColor = "#94000f";
+}
