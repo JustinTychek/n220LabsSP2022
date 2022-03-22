c5c62d8963e6cc03ccf6422b5b00af18de9cd340
Justin Tychek <jmtychek@iu.edu>
2022-03-22 19:36:14 -0400

    Lab 9 exercise 2 completed

--- labs/lab9/ex2/js/app.js
+++ labs/lab9/ex2/js/app.js
@@ -1,2 +1,16 @@
+//Justin Tychek
+//3/22/2022
 
+//define the IDs as a variable
+let dvResults = document.getElementById("dvResults");
+
+//function that generates a random number between 0 and 10
+function randomNumb(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+//click function that calls the random number functin and displays it on the page
+function clickNumb() {
+  let number = randomNumb(0, 10);
+  dvResults.innerHTML = Math.round(number);
 }
