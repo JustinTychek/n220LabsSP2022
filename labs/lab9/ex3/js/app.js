41f2aec39dbb433d54d1ad2c5447a882ca6c7d1f
Justin Tychek <jmtychek@iu.edu>
2022-03-23 09:42:58 -0400

    Lab 9 Completed

--- labs/lab9/ex3/js/app.js
+++ labs/lab9/ex3/js/app.js
@@ -1,1 +1,13 @@
+//Justin Tychek
+//3/22/2022
 
+//define the IDs as a variable
+let dvDisplay = document.getElementById("dvDisplay");
+
+//array that holds the click keywords
+let keywords = ["Home", "About", "Art"];
+
+//onclick function that will choose from the array depending on the number put in the argument.
+function chooseWord(array) {
+  dvDisplay.innerHTML = "You clicked " + keywords[array];
+}
