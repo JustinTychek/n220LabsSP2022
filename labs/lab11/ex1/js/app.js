9fcc4c918d855d8fae3028d2b963d48108cd3e11
Justin Tychek <jmtychek@iu.edu>
2022-04-06 09:52:50 -0400

    Finished exercises 1 and 3

--- labs/lab11/ex1/js/app.js
+++ labs/lab11/ex1/js/app.js
@@ -1,2 +1,18 @@
 //Justin Tychek
 //4/6/2022
+
+//take the ids and make them variables
+let rSquare = document.getElementById("rSquare");
+let gSquare = document.getElementById("gSquare");
+let bSquare = document.getElementById("bSquare");
+
+//add event listeners for when the square is clicked
+rSquare.addEventListener("click", changeColor);
+gSquare.addEventListener("click", changeColor);
+bSquare.addEventListener("click", changeColor);
+
+//function that changes the color of the square using the data attribute
+function changeColor(event) {
+  let color = event.target.getAttribute("data-color");
+  event.target.style.backgroundColor = color;
+}
--- labs/lab11/ex3/js/app.js
+++ labs/lab11/ex3/js/app.js
@@ -1,2 +1,20 @@
 //Justin Tychek
 //4/6/2022
+
+//take the ids and make them variables
+let question1 = document.getElementById("question1");
+let question2 = document.getElementById("question2");
+let question3 = document.getElementById("question3");
+let answerDiv = document.getElementById("answerDiv");
+
+//add event listener for when the button is clicked
+question1.addEventListener("click", listAnswer);
+question2.addEventListener("click", listAnswer);
+question3.addEventListener("click", listAnswer);
+
+//function that lists the answer to the question in a div using the data attribute
+function listAnswer(event) {
+  answer = event.target.getAttribute("data-answer");
+  answerDiv.innerHTML = "";
+  answerDiv.innerHTML = answer;
+}
